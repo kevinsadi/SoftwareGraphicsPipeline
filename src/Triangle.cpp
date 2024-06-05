41f3792c8262cdd15533e5a06eb2acd33c0340a2
kevin_sadi <kevinsadi@live.com>
2024-06-05 07:21:05 -0700

    finish the triangle class (normal, color, getters, setters)

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -1,19 +1,34 @@
 #include "Triangle.hpp"
 
-Triangle::Triangle()
-{
+const Eigen::Vector3f Triangle::ERROR_COLOR(255, 0, 220);
 
+Triangle::Triangle() :
+    m_a(Eigen::Vector3f(0)), m_b(Eigen::Vector3f(0)), m_c(Eigen::Vector3f(0)), m_color(ERROR_COLOR) 
+{
 }
 
-Triangle::Triangle(Eigen::Vector3f a, Eigen::Vector3f b, Eigen::Vector3f c)
+Triangle::Triangle(const Eigen::Vector3f &a, const Eigen::Vector3f &b, const Eigen::Vector3f &c) :
+    m_a(a), m_b(b), m_c(c), m_color(ERROR_COLOR)
 {
     m_a = a;
     m_b = b;
     m_c = c;
 }
 
-
-Triangle::~Triangle()
+void Triangle::computeNormal()
 {
-    
+    // assuming right handed rule (counter clockwise winding)
+    m_normal = m_a.cross(m_b);
 }
+
+void Triangle::setColor(const Eigen::Vector3f &color)
+{
+    m_color = color;
+}
+
+
+Eigen::Vector3f Triangle::getNormal() const
+{
+    return m_normal;
+}
+
--- src/Triangle.hpp
+++ src/Triangle.hpp
@@ -14,7 +14,13 @@ private:
 
 public:
     Triangle();
-    Triangle(Eigen::Vector3f a, Eigen::Vector3f b, Eigen::Vector3f c);
-    ~Triangle();
-    
+    Triangle(const Eigen::Vector3f &a, const Eigen::Vector3f &b, const Eigen::Vector3f &c);
+    ~Triangle() = default;
+
+    void computeNormal();
+    Eigen::Vector3f getNormal() const;
+    void setColor(const Eigen::Vector3f &color);
+
+    // let's make this a const static member so it is shared among all instances of the triangle class
+    static const Eigen::Vector3f ERROR_COLOR;
 };
